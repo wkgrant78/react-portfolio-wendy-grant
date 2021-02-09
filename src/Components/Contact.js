c98e2918d41579a00295165c27678376bd363629
Wendy Grant <wendykgrant.tx@gmail.com>
2021-02-09 15:17:13 -0600

    updated contact info

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -79,11 +79,11 @@ const Contact = ({ data }) => {
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
+					   <h4>Contact Information</h4>
 					   <p className="address">
 						   {data?.name}<br />
 						   {data?.address.city}, {data?.address.state} {data?.address.zip}<br />
-						   <span>{data?.phone}</span>
+						   <span>{data?.email}</span>
 					   </p>
 				   </div>
 
